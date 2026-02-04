7d80d3bd2fa2f827671ef004618bbd2f79c4ef36
Asier-Laiz <asier.laiz@proquinorte.com>
2026-02-04 09:09:15 +0100

    ariketa3

---
+++ ariketa3/app/Models/Student.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $filleable = [
+        'izena','abizena','adina','group_id'
+    ];
+    public function group(){
+        return $this->belongsTo(Group::class);
+    }
+}
